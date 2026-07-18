81d54e6b2ab5a5d7bd10a80427f96d3fb951f5f9
lakkoju-siddhardha <siddhulakkoju54@gmail.com>
2026-07-18 08:44:34 +0530

    Configure Playwright for Render deployment

--- automation/browser.js
+++ automation/browser.js
@@ -3,13 +3,23 @@ const { chromium } = require("playwright");
 async function launchBrowser() {
 
     const browser = await chromium.launch({
-        headless: false,
-        slowMo: 100
+
+        headless: true,
+
+        args: [
+
+            "--no-sandbox",
+            "--disable-setuid-sandbox",
+            "--disable-dev-shm-usage"
+
+        ]
+
     });
 
     const page = await browser.newPage();
 
     return { browser, page };
+
 }
 
 module.exports = launchBrowser;
